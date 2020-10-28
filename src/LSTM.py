7d7166ae164654045fb0ee9fcad7f0b90ab8788a
Dylan Mooers <dmooers@calpoly.edu>
2020-10-28 10:19:29 -0700

    Changed to binary(ish) output

--- src/LSTM.py
+++ src/LSTM.py
@@ -10,18 +10,19 @@ class LSTM :
     def __init__(self, ticker):
         self.ticker = ticker
         self.trainer = Trainer(ticker)
-        self.trainer.generateTrainData(60)
-        self.trainer.generateTestData(60)
+        self.trainer.generateTrainData(30)
+        self.trainer.generateTestData(30)
 
         self.model = tf.keras.Sequential()
-        self.model.add(tf.keras.layers.LSTM(20, input_shape=(60, 4), return_sequences=True))
+        self.model.add(tf.keras.layers.LSTM(20, input_shape=(30, 5), return_sequences=True))
         self.model.add(tf.keras.layers.LSTM(20))
-        self.model.add(tf.keras.layers.Dense(1, activation=LeakyReLU(alpha=0.3)))
+        self.model.add(tf.keras.layers.Dense(1, activation=tf.nn.tanh))
         #self.model.add(LeakyReLU(alpha=0.3)) tf.nn.tanh
         self.model.compile(optimizer='adam', loss='mean_squared_error')
     
     def trainModel(self):
-        #print(self.trainer.Y_train)
+        print(self.trainer.Y_train)
+        print(self.trainer.X_train)
         self.model.fit(self.trainer.X_train, self.trainer.Y_train, epochs=100)
     
     def evaluateModel(self):
@@ -30,10 +31,15 @@ class LSTM :
     def testModel(self):
         predicted = self.model.predict(self.trainer.X_test)
         plt.figure(figsize=(15,6))
-        #plt.plot(predicted.flatten(), label="predicted")
-        plt.plot(self.trainer.Y_test, label="Actual")
-        plt.plot(predicted.flatten(), label='Predicted')
-        plt.ylabel('Predicted')
+        plt.plot(predicted.flatten(), label="predicted")
+        plt.plot(self.trainer.Y_test, label='actual')
+        #predicted = predicted.flatten()
+        #correctness = abs(predicted - self.trainer.Y_test) / self.trainer.Y_test
+        #print(correctness.mean())
+        #print(right.mean())
+        #print(((predicted / abs(predicted)) == self.trainer.Y_test).mean())
+        #print(self.trainer.Y_test)
+        #plt.ylabel('Predicted')
         plt.xlabel('Actual')
         plt.legend()
         plt.show()
